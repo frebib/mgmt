2ae0bd1e24c607de2783ddb8ec2acde17c323446
Guilhem Lettron <guilhem@barpilot.io>
2021-09-04 13:43:36 +0100

    misc: Use go mod
    Update package imports to reflect go-mod dependency versions.
    
    Update to etcd/v3. etcd upstream versioning doesn't play well with Go
    modules or newer versions of gRPC. v3.5.0 and newer follow Go semantic
    versioning guidelines and resolves most versioning conflicts with
    co-dependent libraries. There are many issues surrounding this, see [1].
    
    Fix test-examples for Go Modules. Previously this test script symlinked
    the examples source directory outside of the mgmt tree, but this doesn't
    work with Go modules. Switch to building the example programs in-place,
    and simplify the script to reduce the amount of superfluous operations.
    
    Fixes a couple shellcheck warnings in test-examples too.
    
    [1]: https://github.com/etcd-io/etcd/issues/12124
    
    Signed-off-by: Joe Groocock <me@frebib.net>

--- test/test-examples.sh
+++ test/test-examples.sh
@@ -14,28 +14,18 @@ failures=''
 # TODO: test examples/lang/ directory to see if the .mcl files compile correctly
 
 buildout='test-examples.out'
-# make symlink to outside of package
-linkto="`pwd`/examples/lib/"
-tmpdir="`$mktemp --tmpdir -d tmp.XXX`"	# get a dir outside of the main package
-cd "$tmpdir"
-ln -s "$linkto"	# symlink outside of dir
-cd `basename "$linkto"`
+
+trap 'rm -fv "$buildout"' EXIT # clean up build mess
 
 # loop through individual *.go files in working dir
-for file in `find . -maxdepth 9 -type f -name '*.go'`; do
-	#echo "running test on: $file"
-	run-test go build -i -o "$buildout" "$file" || fail_test "could not build: $file"
+find examples/lib -maxdepth 9 -type f -name '*.go' | while read -r file; do
+	echo "running test on: $file"
+	run-test go build -o "$buildout" "$file" || fail_test "could not build: $file"
 done
-rm "$buildout" || true	# clean up build mess
-
-cd - >/dev/null	# back to tmp dir
-rm `basename "$linkto"`
-cd ..
-rmdir "$tmpdir"	# cleanup
 
 if [[ -n "$failures" ]]; then
 	echo 'FAIL'
-	echo "The following tests (in: ${linkto}) have failed:"
+	echo "The following tests have failed:"
 	echo -e "$failures"
 	echo
 	exit 1
